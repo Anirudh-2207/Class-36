06479145ad441ea8491fa62159e99200fcdf3a1c
Anirudh-2207 <71263159+Anirudh-2207@users.noreply.github.com>
2020-11-06 16:50:42 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,19 @@
+class Player{
+    constructor(){
+
+    }
+    
+    getCount(){
+        var playerCountRef = database.ref("playerCount");
+        playerCountRef.on("value", function(data){
+            playerCount = data.val()
+        })
+    }
+
+
+
+
+
+
+
+}
---
+++ sketch.js
@@ -0,0 +1,19 @@
+var gameState = 0;
+var database;
+var playerCount;
+var form,player,game;
+
+
+function setup(){
+database = firebase.database();
+    createCanvas(500,500);
+   
+}
+
+
+function draw(){
+    background("white");
+ 
+    drawSprites();
+}
+
